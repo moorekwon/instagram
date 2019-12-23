7e70a9bc0f0db44ab1b8e281a80acc39df7b2476
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-23 18:34:35 +0900

    191223 tried but failed to make it operate!

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,29 +1,33 @@
 from django.db import models
 
+from app.config import settings
+
 
 class Post(models.Model):
     """
     인스타그램의 포스트
     """
-    author =
-    content =
-    like_users = 'PostLike를 통한 Many-to-many 구현'
-    created =
-    pass
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    # PostLike를 통한 Many-to-many 구현
+    like_users = models.ManyToManyFielddjango.contrib.auth.models
+    created = models.DateTimeField(auto_now_add=True)
 
 
 class PostImage(models.Model):
     """
     각 포스트의 사진
     """
-    pass
+    post = models.ForeignKey
+    image = models.ImageField
 
 
 class PostComment(models.Model):
     """
     각 포스트의 댓글 (Many-to-one)
     """
-    pass
+    author = models.ForeignKey
+    content = models.TextField
 
 
 class PostLike(models.Model):
@@ -32,4 +36,6 @@ class PostLike(models.Model):
     Many-to-many 필드를 중간모델(Intermediate Model)을 거쳐 사용
     언제 생성됐는지 extra field로 저장 (created)
     """
-    pass
+    post = models.ForeignKey
+    user = models.ForeignKey
+    created = models.DateTimeField
