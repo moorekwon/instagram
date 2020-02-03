330e17001ab23aa3e12aff30fd43930fc563a6f7
Hyojin Kwon <raccoonhj33@gmail.com>
2020-02-03 17:06:58 +0900

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-IDENTITY_FILE="$HOME/.ssh/wps12th.pem"
-HOST="ubuntu@13.125.213.68"
-ORIGIN_SOURCE="$HOME/projects/wps12th/instagram"
-DEST_SOURCE="/home/ubuntu/projects/instagram"
+IDENTITY_FILE="$HOME/.ssh/review.pem"
+HOST="ubuntu@54.180.85.133"
+ORIGIN_SOURCE="$HOME/projects/wps12th/instagram/"
+DEST_SOURCE="/home/ubuntu/projects"
 SSH_CMD="ssh -i ${IDENTITY_FILE} ${HOST}"
 
 echo "== runserver 배포 =="
@@ -11,23 +11,31 @@ echo "== runserver 배포 =="
 # 다시 실행되는 서버 만들기
 # > HOST만 바꾸고 이 스크립트를 실행하면 전체 서버가 세팅되고 runserver된 화면 볼 수 있도록 하기
 
+# 서버 초기설정
+echo "apt update & upgrade & autoremove"
+${SSH_CMD} -C 'sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y && apt -y autoremove'
+echo "apt install python3-pip"
+${SSH_CMD} -C 'sudo apt -y install python3-pip'
+
 # pip freeze
 echo "pip freeze"
-"$HOME"/.pyenv/versions/3.7.5/envs/wps-instagram-env/bin/pip freeze > "$HOME"/projects/wps12th/instagram/requirements.txt
+"$HOME"/.pyenv/versions/3.7.5/envs/wps-instagram-env/bin/pip freeze >"$HOME"/projects/wps12th/instagram/requirements.txt
 
 # 기존 폴더 삭제
 echo "1. 기존 폴더 삭제"
-${SSH_CMD} sudo rm -rf S{DEST_SOURCE}
+${SSH_CMD} sudo rm -rf ${DEST_SOURCE}
 #ssh -i ~/.ssh/wps12th.pem ubuntu@13.125.213.68 rm -rf /home/ubuntu/projects/instagram
 
 # 로컬에 있는 파일 업로드
 echo "2. 로컬 파일 업로드"
-scp -q -i "${IDENTITY_FILE}" -r "${ORIGIN_SOURCE}" ${HOST}:${DEST_SOURCE}
+${SSH_CMD} mkdir -p ${DEST_SOURCE}
+scp -i "${IDENTITY_FILE}" -r "${ORIGIN_SOURCE}" ${HOST}:${DEST_SOURCE}
 #scp -i ~/.ssh/wps12th.pem -r ~/projects/wps12th/instagram ubuntu@13.125.213.68:/home/ubuntu/projects/
 
 # pip install
 echo "pip install"
-${SSH_CMD} pip3 install -q -r /home/ubuntu/projects/instagram/requirements
+${SSH_CMD} sudo apt-get install python3-pip
+${SSH_CMD} pip3 install -q -r /home/ubuntu/projects/instagram/requirements.txt
 
 echo "3. Screen 실행"
 # 실행중이던 screen 세션 종료
@@ -39,7 +47,7 @@ ${SSH_CMD} -C 'screen -S runserver -d -m'
 #ssh -i ~/.ssh/wps12th.pem ubuntu@13.125.213.68 -C 'screen -S runserver -d -m'
 
 # 실행중인 세션에 명령어 전달
-${SSH_CMD} -C "screen -r runserver -X stuff $'sudo python3 /home/ubuntu/projects/instagram/app/manage.py runserver 0:80\n'"
+${SSH_CMD} -C "screen -r runserver -X stuff 'sudo python3 /home/ubuntu/projects/instagram/app/manage.py runserver 0:80\n'"
 #ssh -i ~/.ssh/wps12th.pem ubuntu@13.125.213.68 -C "screen -r runserver -X stuff $'sudo python3 /home/ubuntu/projects/instagram/app/manage.py runserver 0:80\n'"
 
 echo "== 배포 완료 =="
